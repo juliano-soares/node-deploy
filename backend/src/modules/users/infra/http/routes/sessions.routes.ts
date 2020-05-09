b5fabaa09cee328197990fea688dadba7c839d76
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2020-05-09 20:51:36 -0300

    testes concluidos.

---
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import SessionsController from '../controllers/SessionsController';
+
+const SessionsRouter = Router();
+const sessionsController = new SessionsController();
+
+SessionsRouter.post('/', sessionsController.create);
+
+export default SessionsRouter;
